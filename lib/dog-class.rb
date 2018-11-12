875b95567709236dde427b32cf21843866f628ea
Erik Feller <enfeller@hollandhart.com>
2018-11-11 21:44:22 -0700

    Broken hash integration

--- lib/dog-class.rb
+++ lib/dog-class.rb
@@ -30,6 +30,9 @@ class Dog
     @status
     attr_accessor :intake_time
     @intake_time
+    attr_accessor :hold_times
+    @hold_times
+    attr_accessor :out_time
     @out_time
 
     def load_data(id_query, db_handle)
@@ -40,10 +43,10 @@ class Dog
         #Change Attributes in case we need to
     end
 
-    def store_data(db_loc)
+    def store_data(db_handle)
         #Store the data of the current object 
-        db_handle = SQLite3::Database.new(db_loc)
-        if db_handle.execute("SELECT name FROM dogdata WHERE id LIKE " + id.to_s).length == 0
+        if is_new(db_handle)
+           puts("dog " + name + " is a new dog")
            db_handle.execute("INSERT INTO dogdata(id, name, breed_primary, breed_secondary, gender, age, status) VALUES(?, ?, ?, ?, ?, ?, ?)", [id, name, breed_primary, breed_second, gender, age, status])
         end
         db_handle.execute("INSERT INTO seconddata(id, time, status) VALUES(?, ?, ?)", [id, intake_time, status])
@@ -51,6 +54,23 @@ class Dog
 
     def store_data_direct(id, breed_primary, breed_second, gender, age, status, intake_time)
         #Add data into the database
+    end
+
+    def is_new(db_handle)
+      #determine if a dog is new to the database or not
+      if db_handle.execute("SELECT id FROM dogdata WHERE id LIKE " + id.to_s).length == 0
+        true
+      else
+        false
+      end
+    end
+
+    def status_changed(db_loc)
+      #determine if the status of a dog has been changed
+    end
+
+    def hold_time_record(db_loc)
+      #record the specific times a dog has been placed on hold. Stored as comma seperated timestamps
     end
         
 end
--- main.rb
+++ main.rb
@@ -6,18 +6,19 @@ require 'time'
 require './lib/dog-class.rb'
 
 @main_db_loc='data/main.sqlite'
+working_map=Hash.new
 
 
-def web_fetch
+def web_fetch(db_handle, working_map)
     #Check for the Databases
     database_check()
     #Open the page and grab the relevant elements.
     page = Nokogiri::HTML(open("https://www.boulderhumane.org/animals/adoption/dogs")) 
     rows = page.css("div.views-row")
-    curr_dog = Dog.new
     #iterate over each of the entries
     for dog in rows
         #attribs = dog.css("div.views-field") #don't need rn but might later
+        curr_dog = Dog.new
         curr_dog.id = dog.css("div.views-field.views-field-field-pp-animalid-1").css("span.field-content").text.strip.to_i
         curr_dog.name = dog.css("div.views-field.views-field-field-pp-animalname").text.strip
         curr_dog.breed_primary = dog.css("div.views-field.views-field-field-pp-primarybreed").text.strip
@@ -27,8 +28,10 @@ def web_fetch
         curr_dog.gender = dog.css("div.views-field.views-field-field-pp-gender").css("span.field-content").text.strip
         curr_dog.status= dog.css("div.views-field.views-field-field-pp-splashtitle").text.strip
         curr_dog.intake_time = Time.now.utc.to_i
+        #write to HM
+        working_map[curr_dog.id]=curr_dog
         #write to database
-        curr_dog.store_data(@main_db_loc)
+        curr_dog.store_data(db_handle)
     end
 end
 
@@ -36,10 +39,22 @@ def database_check
     if !(File.exist?@main_db_loc)
         puts "Main DB not found. Creating one now..."
         main_db = SQLite3::Database.new(@main_db_loc)
-        main_db.execute("CREATE TABLE dogdata(id, name, breed_primary, breed_secondary, gender, age, status)")
+        main_db.execute("CREATE TABLE dogdata(id, name, breed_primary, breed_secondary, gender, age, status, in_time, hold_times, out_time)")
         main_db.execute("CREATE TABLE seconddata(id, time, status)")
     end
 end
 
-web_fetch()
-
+if !(File.exist?@main_db_loc)
+  puts "Main DB not found. Creating one now..."
+  main_db = SQLite3::Database.new(@main_db_loc)
+  main_db.execute("CREATE TABLE dogdata(id, name, breed_primary, breed_secondary, gender, age, status, in_time, hold_times, out_time)")
+  main_db.execute("CREATE TABLE seconddata(id, time, status)")
+end
+main_db = SQLite3::Database.new(@main_db_loc)
+web_fetch(main_db, working_map)
+unknown = main_db.execute("SELECT * FROM dogdata WHERE out_time IS NULL") do |row|
+  p ("this is a row " + row[1])
+end
+working_map.each do |key, val| 
+  puts(val.name)
+end
